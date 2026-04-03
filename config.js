878956b82c668e1ec1a55deda9b833c6d628a249
DominicDev <s0l0estudi4r@gmail.com>
2026-04-03 20:04:41 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SHIP WREK';
+export let nameSongs = [
+	"VESSEL", /*1*/
+	"ARK", /*2*/
+	"PAIN", /*3*/
+	"FOOLS GOLD", /*4*/
+	"WAITING", /*5*/
+	"STRANDED", /*6*/
+	"VESSEL (SLOWED + REVERBED)", /*7*/
+	"ONCE I WAKE", /*8*/
+	"SINKING FEEL", /*9*/
+	"EVANESCENT", /*10*/
+	"SO GLUM", /*11*/
+	"BREAK THE SOUND", /*12*/
+	" SYSTEM OVERLOAD", /*13*/
+	"REPRISAL", /*14*/
+	"UNDERGROUND", /*15*/
+	"THE GRID", /*16*/
+	"WHEN YOUR VIBING", /*17*/
+	"DOOZY", /*18*/
+	"REDEYE", /*19*/
+	"SHADOWS", /*20*/
+]
